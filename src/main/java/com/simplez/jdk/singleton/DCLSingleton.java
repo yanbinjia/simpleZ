cda323f80bb7f9f8a24bac5abe78540976fb6ea2
yanbin.jia <yanbinjia@126.com>
2017-08-22 14:30:25 +0800

    ddd

--- src/main/java/com/simplez/jdk/singleton/DCLSingleton.java
+++ src/main/java/com/simplez/jdk/singleton/DCLSingleton.java
@@ -1,7 +1,10 @@
 package com.simplez.jdk.singleton;
 
-// 双检锁/双重校验锁（DCL，即 double-checked locking）
-// 这种方式采用双锁机制，安全且在多线程情况下能保持高性能。
+/*
+ * 双检锁/双重校验锁（DCL，即 double-checked locking）
+ * 这种方式采用双锁机制，安全且在多线程情况下能保持高性能。
+ * 
+ */
 public class DCLSingleton {
 	private volatile static DCLSingleton singleton;
 
